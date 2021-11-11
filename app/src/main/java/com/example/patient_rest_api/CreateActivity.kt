bb1e43d0393240b0444878bfb31894eb4b9f030a
Poojan Patel <poojan.patel1126@gmail.com>
2021-11-11 15:52:49 -0800

    Android Integration through heroku link

---
+++ app/src/main/java/com/example/patient_rest_api/CreateActivity.kt
@@ -0,0 +1,70 @@
+package com.example.patient_rest_api
+/*
+Group-2
+Milestone-3
+    Poojan Jitendrakumar Patel [301228811]
+    Matthew Maxwell [301200258]
+    Sanjeevan Pushparaj [301213104]
+*/
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+
+class CreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create)
+    }
+
+    // Function for button send
+    fun clickCreate(view: android.view.View) {
+
+        if (view.id == R.id.buttoncreate) {
+
+            // Collect data from user
+            val name = findViewById<EditText>(R.id.ETname).text.toString()
+            val lastname = findViewById<EditText>(R.id.ETlname).text.toString()
+            val address = findViewById<EditText>(R.id.ETaddress).text.toString()
+            val dob = findViewById<EditText>(R.id.ETdob).text.toString()
+            val department = findViewById<EditText>(R.id.ETdep).text.toString()
+            val doctor = findViewById<EditText>(R.id.ETdoc).text.toString()
+
+            // Instantiate the RequestQueue.
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://patient-rest-api.herokuapp.com/patients"
+            val textView = findViewById<TextView>(R.id.details)
+
+            // Post parameters
+            val params = HashMap<String, String>()
+            params["first_name"] = name
+            params["last_name"] = lastname
+            params["address"] = address
+            params["date_of_birth"] = dob
+            params["department"] = department
+            params["doctor"] = doctor
+
+            val jsonObject = JSONObject(params as Map<*, *>)
+
+            // Request a string response from the provided URL.
+            val request = JsonObjectRequest(
+                Request.Method.POST, url, jsonObject,
+                Response.Listener { response ->
+                    // Display the first 500 characters of the response string.
+                    textView.text = "Response: %s".format(response.toString())
+                },
+                Response.ErrorListener { textView.text = "That didn't work!" })
+
+            // Add the request to the RequestQueue.
+            queue.add(request)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/patient_rest_api/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.patient_rest_api
+/*
+Group-2
+Milestone-3
+    Poojan Jitendrakumar Patel [301228811]
+    Matthew Maxwell [301200258]
+    Sanjeevan Pushparaj [301213104]
+*/
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // Get all patient data
+    fun btnget(view: android.view.View) {
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://patient-rest-api.herokuapp.com/patients"
+        val textView = findViewById<TextView>(R.id.textView2)
+
+        // Request a string response from the provided URL.
+        val request = JsonArrayRequest(Request.Method.GET,url,null,
+            Response.Listener { response ->
+                // Display the first 500 characters of the response string.
+                //textView.text = "Response is: ${response.substring(0, 500)}"
+                textView.text = "Response: %s".format(response.toString())
+
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+        // Add the request to the RequestQueue.
+        queue.add(request)
+
+    }
+
+    // Add new patient
+    fun btnpost(view: android.view.View) {
+
+        if (view.id == R.id.btnpost) {
+            // Create an intent object
+            val intent = Intent(this@MainActivity, CreateActivity::class.java)
+
+            // Start Subactivity
+            startActivity(intent)
+        }
+
+    }
+}
